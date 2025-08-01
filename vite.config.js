0b54ec635135354338dfc662f64caa069dcca5c0
Sachin Thilakarathna <thilakarathnasachin2003@gmail.com>
2025-08-01 20:05:08 +0530

    Update site content

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/', // Important: this is correct for user/organization sites
+
 })
